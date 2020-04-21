50f43383f0b53224d4adb8207b9effad53783543
lalize <printf@kakao.com>
2020-04-21 14:35:40 +0900

    feat: mvc 연습

--- src/main/java/wooteco/helloworld/controller/UserController.java
+++ src/main/java/wooteco/helloworld/controller/UserController.java
@@ -1,5 +1,14 @@
 package wooteco.helloworld.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import wooteco.helloworld.domain.User;
 
@@ -10,28 +19,32 @@ public class UserController {
     /**
      * GET /users
      */
-    public String sayHi(String name) {
+    @GetMapping("/users")
+    public String sayHi(@RequestParam String name) {
         return "Hi " + name;
     }
 
     /**
      * GET /users/{id}
      */
-    public String retrieveUser(Long id) {
+    @GetMapping("/users/{id}")
+    public String retrieveUser(@PathVariable Long id) {
         return "user's id is " + id;
     }
 
     /**
      * POST /users
      */
-    public String createUserWithJson(User user) {
+    @PostMapping("/users")
+    public String createUserWithJson(@RequestBody User user) {
         return "Hello " + user.getName();
     }
 
     /**
      * POST /users/form
      */
-    public String createUserWithForm(Map<String, String> paramMap) {
+    @PostMapping("/users/form")
+    public String createUserWithForm(@RequestParam Map<String, String> paramMap) {
         return "Hello " + paramMap.get("name");
     }
 
--- src/main/java/wooteco/helloworld/controller/WelcomeController.java
+++ src/main/java/wooteco/helloworld/controller/WelcomeController.java
@@ -1,14 +1,22 @@
 package wooteco.helloworld.controller;
 
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import wooteco.helloworld.domain.User;
 
+@Controller
 public class WelcomeController {
     /**
      * GET /welcome
      */
+    @GetMapping("/welcome")
+    @ResponseBody
     public String welcome() {
         return "welcome";
     }
@@ -16,13 +24,16 @@ public class WelcomeController {
     /**
      * GET /welcome-page
      */
-    public String welcomePage() {
-        return "welcome";
+    @GetMapping("/welcome-page")
+    public String welcomePage(@RequestParam(defaultValue = "brown") String name, Model model) {
+        model.addAttribute("name", name);
+        return"welcome";
     }
 
     /**
      * GET /welcome-user
      */
+    @GetMapping("/welcome-user")
     public ResponseEntity welcomeUser() {
         return ResponseEntity.ok(new User("브라운", "brown@email.com", 20));
     }
